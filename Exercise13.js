fd38efebec8dd98e23a867f1072b81fc5808149d
Jordan Parker <parkerj4321@gmail.com>
2018-07-31 11:39:35 -0500

    Exercise 13 & 14

---
+++ Exercise13.js
@@ -0,0 +1,13 @@
+function main(){
+    alertHello();
+    alertWorld();
+}
+
+function alertHello(){
+    alert("Hello");
+}
+function alertWorld() {
+    alert("World");
+}
+
+main();
---
+++ Exercise14.js
@@ -0,0 +1,10 @@
+function main(){
+    var favoriteTeacher = "Kevin";
+    favoriteTeach(favoriteTeacher);
+}
+
+function favoriteTeach(teacher) {
+    alert(teacher);
+}
+
+main();
